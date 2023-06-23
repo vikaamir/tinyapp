0927a02a1df59215a9d281b0766d5b5c502842b7
Victoria Amirov <vika.amir20@gmail.com>
2023-06-22 23:06:59 -0700

    and a new form with POST

--- express_server.js
+++ express_server.js
@@ -3,6 +3,9 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+function generateRandomString() {}
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -26,6 +29,9 @@ app.get("/urls", (req, res) => {
    const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars)
 });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls/:id", (req, res) => {
   console.log(req.params)
@@ -33,6 +39,11 @@ app.get("/urls/:id", (req, res) => {
   console.log(templateVars)
   res.render("urls_show", templateVars);
 });
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
  
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
