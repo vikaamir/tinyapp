02a2bebcf57f2664c398301bf8b00ade1cf1d433
Victoria Amirov <vika.amir20@gmail.com>
2023-06-29 19:45:00 -0700

    clean the code

--- express_server.js
+++ express_server.js
@@ -1,17 +1,17 @@
 const { name } = require("ejs");
 const express = require("express");
 const app = express();
-const cookie = require('cookie-parser')
+const cookie = require('cookie-parser');
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-app.use(cookie())
+app.use(cookie());
 
 function generateRandomString() {
   // found the solution on stackOverFlow
   return Array.from(Array(6), () => Math.floor(Math.random() * 36).toString(36)).join('');
-};
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -29,10 +29,10 @@ const users = {
     password: "dishwasher-funk",
   },
 };
-// helper function to find the email 
+// helper function to find the email
 function emailExists(email) {
-  return Object.values(users).find(user => user.email === email)
-};
+  return Object.values(users).find(user => user.email === email);
+}
 // app.get("/", (req, res) => {
 //   res.send("Hello!");
 // });
@@ -51,7 +51,7 @@ app.get("/urls", (req, res) => {
     user: users[req.cookies.user_id],
     urls: urlDatabase
   };
-  console.log(templateVars)
+  console.log(templateVars);
   res.render("urls_index", templateVars);
 });
 
@@ -97,7 +97,7 @@ app.post("/urls/:id/delete", (req, res) => {
 
 app.post("/urls/:id/edit", (req, res) => {
   const editID = req.params.id;
-  const longURL = req.body.longURL// goes to the longUrl that we creat in urls_show 
+  const longURL = req.body.longURL;// goes to the longUrl that we creat in urls_show
   urlDatabase[editID] = longURL;
   res.redirect("/urls");
 });
@@ -112,33 +112,33 @@ app.get("/urls/:id/edit", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-// checks if the email and the password are macthing 
+// checks if the email and the password are macthing
 app.post("/login", (req, res) => {
-  const email = req.body.email
-  const password = req.body.password
-  const user = emailExists(email)// using the existed function for find the user
+  const email = req.body.email;
+  const password = req.body.password;
+  const user = emailExists(email);// using the existed function for find the user
   if (!user) {
     return res.status(403).send("E-mail is not found");
   } else {
     if (user["password"] !== password) {
       return res.status(403).send("Password is not macthing");
     }
-    res.cookie("user_id", user.id)
+    res.cookie("user_id", user.id);
     res.redirect("/urls");
   }
 });
 
 app.get("/login", (req, res) => {
   //add user cookie data to templateVars so that header can render correct state
-  const templateVars = { 
-   user: users[req.cookies.user_id]
+  const templateVars = {
+    user: users[req.cookies.user_id]
   };
   res.render("login", templateVars);
 });
 
 //Clears the cookie specified by id.
 app.post("/logout", (req, res) => {
-  res.clearCookie("user_id")//
+  res.clearCookie("user_id");
   res.redirect("/login");
 });
 
@@ -149,24 +149,24 @@ app.get("/register", (req, res) => {
   res.render("register", templateVars);
 });
 
-//creat an object for new regustered user and give user id 
+//creat an object for new regustered user and give user id
 app.post("/register", (req, res) => {
   const newID = generateRandomString();
-  const newUser = { 
+  const newUser = {
     email: req.body.email,
     password: req.body.password,
     id: newID
   };
   // find if email or password alredy exists
-  if(!req.body.email || !req.body.password || emailExists(req.body.email)) {
-    return res.status(400).send("Email or password alredy exists"); 
+  if (!req.body.email || !req.body.password || emailExists(req.body.email)) {
+    return res.status(400).send("Email or password alredy exists");
   } else {
     users[newID] = newUser;
-  res.cookie("user_id", newID);
-    res.redirect("/urls")
+    res.cookie("user_id", newID);
+    res.redirect("/urls");
 
-    }
-  });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
