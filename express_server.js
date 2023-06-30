0c929165705963678b644bee5a3f3f8fce81503a
Victoria Amirov <vika.amir20@gmail.com>
2023-06-30 16:07:43 -0700

    error message if the id does not exist

--- express_server.js
+++ express_server.js
@@ -55,13 +55,28 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-app.get("/urls/new", (req, res) => {
-  const templateVars = {
-    user:users[req.cookies.user_id]
-  };
-  res.render("urls_new", templateVars);
+app.post("/urls", (req, res) => {
+  const userID  = req.cookies.user_id
+  if (!userID){
+    return res.status(403).send("To create a short URL, please log-in first!");
+  }
+  console.log(req.body); // Log the POST request body to the console
+  let newID = generateRandomString();
+  urlDatabase[newID] = req.body["longURL"];//add new key value to database
+  res.redirect(`/urls/${newID}`);
 });
 
+app.get("/urls/new", (req, res) => {
+  const userID  = req.cookies.user_id
+  if (!userID){
+    res.redirect("/login")
+  } else {
+    const templateVars = {
+      user:users[req.cookies.user_id]
+    };
+  res.render("urls_new", templateVars);
+  }
+});
 
 app.get("/urls/:id", (req, res) => {
   //req.params is a object with route parameter in it witch in this case id is in it
@@ -75,17 +90,17 @@ app.get("/urls/:id", (req, res) => {
 });
 
 app.get("/u/:id", (req, res) => {
-  //To be able to get id use req.params
+  const userID  = req.cookies.user_id
   const longURL = urlDatabase[req.params.id];//to get the correct longUrl access database with the id
+  if (!userID) {
+    return res.status(403).send(`This page  is not in the database`);
+
+
+  }
   res.redirect(longURL);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  let newID = generateRandomString();
-  urlDatabase[newID] = req.body["longURL"];//add new key value to database
-  res.redirect(`/urls/${newID}`);
-});
+
 
 app.post("/urls/:id/delete", (req, res) => {
   const deleteID = req.params.id;
@@ -149,7 +164,6 @@ app.get("/register", (req, res) => {
   };
   res.render("register", templateVars);
   res.redirect("/urls");
-
 });
 
 //creat an object for new regustered user and give user id
